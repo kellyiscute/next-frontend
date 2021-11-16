5167ea137205370eae34b23ff2fb782a210fec41
kelly <guojiyou40020@hotmail.com>
2021-11-16 23:48:32 +0800

    moved script tag out of head

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,21 +39,21 @@ const Home: NextPage<IHomepageProps> = ({ wakatimeSummary }) => {
   useEffect(() => {}, []);
   return (
     <div>
+      <Script>
+        {`(function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:2704139,hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+      </Script>
       <Head>
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.14.0/devicon.min.css"
         />
-        <Script>
-          {`(function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:2704139,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
-        </Script>
       </Head>
       <div className={classNames.root}>
         <Header />
