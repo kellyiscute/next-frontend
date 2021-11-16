5aba1b9ffb6d4e7ad51e752acf8d625646e8f92d
kelly <guojiyou40020@hotmail.com>
2021-11-17 00:21:56 +0800

    use google analytics instead

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,15 +39,16 @@ const Home: NextPage<IHomepageProps> = ({ wakatimeSummary }) => {
   useEffect(() => {}, []);
   return (
     <div>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-1FT0N56JB1"
+      />
       <Script id="hj">
-        {`(function(h,o,t,j,a,r){
-          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-          h._hjSettings={hjid:2704139,hjsv:6};
-          a=o.getElementsByTagName('head')[0];
-          r=o.createElement('script');r.async=1;
-          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-          a.appendChild(r);
-        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-1FT0N56JB1');`}
       </Script>
       <Head>
         <link
