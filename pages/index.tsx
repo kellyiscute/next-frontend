b69372783c3ee4b3cf0ca68516646a74bfa129d9
Kelly <guojiyou40020@hotmail.com>
2021-11-16 13:25:50 +0800

    commented github summary for building

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,24 +5,24 @@ import Header from "../components/header";
 import classNames from "../styles/Home.module.scss";
 import { IWakatimeSummary } from "models/wakatime/summary";
 import WakaTimeSummary from "components/wakatime-summary";
-import { Octokit } from "octokit";
+// import { Octokit } from "octokit";
 
 export const getStaticProps: GetStaticProps = async () => {
   const wakatimeSummary = await getWakaTimeSummary();
-  const octokit = new Octokit({ auth: process.env.GITHUB_PAT });
-  const repos = await octokit.request("GET /users/{username}/repos", {
-    username: "guo40020",
-    per_page: 100,
-  });
-  let commits = 0;
-  const totalChanges = {};
-  for (const repo of repos.data) {
-    const repoData = await octokit.request(
-      "GET /repos/{owner}/{repo}/stats/contributors",
-      { owner: "guo40020", repo: repo.name }
-    );
-    // commits += repoData.data.
-  }
+  // const octokit = new Octokit({ auth: process.env.GITHUB_PAT });
+  // const repos = await octokit.request("GET /users/{username}/repos", {
+  //   username: "guo40020",
+  //   per_page: 100,
+  // });
+  // let commits = 0;
+  // const totalChanges = {};
+  // for (const repo of repos.data) {
+  //   const repoData = await octokit.request(
+  //     "GET /repos/{owner}/{repo}/stats/contributors",
+  //     { owner: "guo40020", repo: repo.name }
+  //   );
+  //   // commits += repoData.data.
+  // }
   return {
     props: { wakatimeSummary },
     revalidate: 100,
